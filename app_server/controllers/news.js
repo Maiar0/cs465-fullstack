f664523c0d16c01ec19e91b842099f688f74eb0c
Dennis Ward <dennis.ward1@snhu.edu>
2025-01-25 12:42:23 -0600

    Module 3 Baseline

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,11 +1,14 @@
+var fs = require('fs');
+var sidebar = JSON.parse(fs.readFileSync('./data/sidebar.json', 'utf8'));
+
 /* GET news view */
 const news = (req, res) => {                    
-    res.render('news', { title: 'Travlr Getaways' ,
-      active_nav: {
-        news: true
-      } 
-    });          
-  };
-  module.exports = {                               
+    res.render(
+      'news', 
+      { title: 'Travlr Getaways', sidebar, active_nav: { news: true } }
+    );          
+};
+
+module.exports = {                               
    news                                           
-  };                                               
+};
